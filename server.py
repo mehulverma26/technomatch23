83d2f4b2cabdfcaf1dd160c0ec51d054394c6d86
Mehul Verma <mehulverma04@gmail.com>
2023-03-31 00:34:20 +0530

    presentation

--- server.py
+++ server.py
@@ -1,8 +1,13 @@
-from flask import Flask, render_template, request, Markup
+from flask import Flask, render_template
 app = Flask(__name__)
-@app.route('/')
-def home():
-    title="Home"
-    return render_template('index.html', title=title)
-if __name__ == '__main__':
-    app.run(debug=True)
+
+@app.route("/")
+def hello():
+    title="Introduction to the Project"
+    return render_template('index.html',title=title)
+
+@app.route("/prediction")
+def prediction():
+    title="the plots and predictions"
+    return render_template('prediction.html',title=title)
+app.run(debug=True)
