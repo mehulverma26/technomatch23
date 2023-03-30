29eec4f3d11c059def6521c797b5d84114c4bcd3
Mehul Verma <mehulverma04@gmail.com>
2023-03-31 02:37:33 +0530

    adding pages

--- server.py
+++ server.py
@@ -10,4 +10,24 @@ def hello():
 def prediction():
     title="the plots and predictions"
     return render_template('prediction.html',title=title)
+
+@app.route("/humidity")
+def humidity():
+    title="humidity"
+    return render_template('humidity.html',title=title)
+
+@app.route("/temp")
+def temp():
+    title="temperature"
+    return render_template('temp.html',title=title)
+
+@app.route("/snow")
+def snow():
+    title="snow"
+    return render_template('snow.html',title=title)
+
+@app.route("/precipitation")
+def precipitation():
+    title="precipitation"
+    return render_template('precipitation.html',title=title)
 app.run(debug=True)
